e8f43bdad474b12d5db5afc9fca3df1491494d93
Isa Bolling <isaboll1@yahoo.com>
2018-10-08 18:00:30 -0400

    Created main.cpp, and planner.h

---
+++ Planner.h
@@ -0,0 +1,19 @@
+#ifndef PLANNER_H
+#define PLANNER_H
+
+#include <iostream>
+#include <string>
+#include <random>
+#include <cmath>
+
+using namespace std;
+
+class Planner {
+public:
+private:
+};
+
+
+#endif 
+
+
